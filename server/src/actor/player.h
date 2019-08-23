c12553a62ba945bd632f35ce331cc0118cc79220
oceancx <oceancx@gmail.com>
2019-08-23 17:08:49 +0800

    rm code

--- server/src/actor/player.h
+++ server/src/actor/player.h
@@ -15,54 +15,7 @@ public:
 	Player(uint64_t id = 0);
 	virtual ~Player();
 
-	void ResetDirAll(int dir);
-	void ResetDir(int dir);
 
-
-	int GetDirByDegree(float degree);
-	void ReverseDir();
-
-	virtual void OnUpdate(float dt);
-
-
-	void MoveTo(float x, float y);
-
-	void MoveTo(GameMap* gameMapPtr, int destX, int destY);
-	void MoveToRandom();
-
-	int GetDrawY();
-
-	bool IsMove() { return m_IsMove; }
-	void SaveFrame(int index);
-	void ChangeRole(int roleID);
-	void ChangeWeapon(int WeaponID);
-	void ChangeAction(int actionID);
-
-	
-
-	
-	void LogInfo();
-
-	void Say(std::wstring Text);
-	void Say(std::string Text);
-	bool CanMove();
-
-	void AddTarget(Player* target) { m_Targets.push_back(target); }
-	std::vector<Player*> GetTargets() { return m_Targets; }
-	void ClearTargets() { m_Targets.clear(); }
-	float GetWidth();
-	float GetHeight();
-
-	Bound GetScreenBound();
-
-	void SetDir(int dir);
-	void SetActionID(int state);
-
-
-protected:
-
-
-	std::vector<Player*> m_Targets;
 
 };
 
