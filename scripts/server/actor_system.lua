a3d2e64bccf8df92fb092b65c4f79502a2fe227e
oceancx <oceancx@gmail.com>
2020-01-01 23:25:05 +0800

    unfinish

--- scripts/server/actor_system.lua
+++ scripts/server/actor_system.lua
@@ -16,3 +16,22 @@ stub[PTO_C2S_CREATE_PLAYER] = function(req, js)
     local actor = actor_manager_fetch_player_by_id(pid)
     actor:SetProperties(props)
 end
+
+
+
+teams = {}
+stub[PTO_C2S_MAKE_TEAM] = function(req,js)
+    local leader = actor_manager_fetch_player_by_id(req.leader) 
+    local member = actor_manager_fetch_player_by_id(req.member) 
+    local team = {}
+    team.leader = req.leader
+    team.id = os.time()
+    team.members = { req.leader, req.member }
+    table.insert(teams, team)
+    -- leader:SetTeam(team.id)
+    -- member:SetTeam(team.id)
+
+    net_send_message_to_all_players(PTO_S2C_MAKE_TEAM, cjson.encode(team))
+end
+
+
