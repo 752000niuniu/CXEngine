d94b058ae772e6a03a0175506083ac29867bb1ef
oceancx <oceancx@gmail.com>
2020-01-10 18:23:39 +0800

    update

--- scripts/combat/server/combat_system.lua
+++ scripts/combat/server/combat_system.lua
@@ -56,23 +56,257 @@ end
 		技能开始释放（数值结算，表现计算）
 		技能释放过程 
 		技能释放结束（）	
-]]
-
---[[
 	cmd = {
 		master 
 		target 可为空
 		技能id
 	}
 ]]
+
+
+
+__skills__ = __skills__ or {}
+
+local ActorMT = actor_get_metatable()
+function ActorMT:CastSkill(skill_id)
+    local actor = self
+    local skill_tpl = skill_table[skill_id]
+    if not skill_tpl then return end
+    local battle = self:GetBattle()
+    if not battle then return end
+    cxlog_info('cast skill ',skill_id)
+    local skill = SkillMT:new()
+    __skills__[skill.id] = skill
+    self:SetUsingSkill(skill.id)
+
+    skill.caster_end = false
+    skill.target_end = false
+    skill.tid = skill_id
+    skill.turn = battle.turn
+    skill.templ = skill_tpl
+    skill.group_kill = skill.templ.group_kill
+    skill.combo = skill.templ.combo
+    skill.type = skill.templ.type
+    
+    if skill.type == 'atk' then
+        if skill.group_kill > 0 then
+			if IsClient() then
+                on_cast_group_attack(skill, actor)
+            end
+        else
+            skill.atk_cnt = 0
+            skill.atk_damage = {}
+            if skill.combo > 0 then
+                skill.atk_cnt = skill.combo
+            else
+                skill.atk_cnt = 1
+            end
+            local target = actor:GetTarget()
+            for i=1, skill.atk_cnt do
+                local damage = actor:GetAttackDamage(false,false,0,1)
+                table.insert(skill.atk_damage, damage)
+                target:ModifyHP(-damage)
+                if target:IsDead() then
+                    skill.atk_cnt = i
+                    break
+                end
+            end
+			if IsClient() then
+                on_cast_attack(skill, actor)
+            end
+        end
+    elseif skill.type == 'spell' then
+        local to_self_group = false
+        if skill.sub_type == SKILL_SUBTYPE_DEFAULT or skill.sub_type == SKILL_SUBTYPE_SEAL then
+            to_self_group = false
+        elseif skill.sub_type == SKILL_SUBTYPE_HEAL or skill.sub_type == SKILL_SUBTYPE_AUXI then
+            to_self_group = true
+        end
+        local targets = {}
+        for i, actor in ipairs(battle.actors) do
+            if to_self_group then
+                if actor:GetProperty(PROP_TEAM_TYPE) == self:GetProperty(PROP_TEAM_TYPE) then
+                    table.insert(targets,actor)
+                end    
+            else
+                if actor:GetProperty(PROP_TEAM_TYPE) ~= self:GetProperty(PROP_TEAM_TYPE) then
+                    table.insert(targets,actor)
+                end    
+            end
+        end
+        table.sort(targets,function(a,b)
+            return a:CalcSpeed() > b:CalcSpeed()
+        end)
+        
+        if skill.templ.group_kill > 0 then
+            skill.spell_cnt = math.min(#targets,skill.group_kill) 
+            skill.spell_actors = { }
+            for i=1,#targets do
+                local target = targets[i]
+                if target:GetID() ~= self:GetTarget():GetID() and #skill.spell_actors < skill.spell_cnt-1 then
+                    table.insert(skill.spell_actors, target)
+                end
+            end
+            table.insert(skill.spell_actors,self:GetTarget())
+        else
+            skill.spell_cnt = 1
+            skill.spell_actors = {}
+            table.insert(skill.spell_actors,self:GetTarget())
+        end
+        
+        skill.spell_damage = {}
+        for i=1,skill.spell_cnt do
+            local target = skill.spell_actors[i]
+            local damage = actor:GetSpellDamage(target)
+            if skill.templ.sub_type == SKILL_SUBTYPE_SEAL then
+                damage = 0            
+            elseif  skill.templ.sub_type == SKILL_SUBTYPE_HEAL then
+                damage = -damage
+            end
+            table.insert(skill.spell_damage,damage)
+            target:ModifyHP(-damage)
+        end
+
+		skill.atk_anim = skill.templ.atk_anim
+		if IsClient() then
+			on_cast_spell(skill,actor)
+		end
+    end
+end
+
+function ActorMT:SetUsingSkill(skill_uid)
+    self:SetProperty(PROP_USING_SKILL, skill_uid)
+end
+
+function ActorMT:GetUsingSkill()
+    local id = self:GetProperty(PROP_USING_SKILL)
+    return __skills__[id]
+end
+
+function ActorMT:EndUsingSkill()
+    local id = self:GetProperty(PROP_USING_SKILL)
+    __skills__[id] = nil
+    self:SetProperty(PROP_USING_SKILL, 0)
+end
+
+function ActorMT:IsUsingSkill()
+    return self:GetProperty(PROP_IS_USING_SKILL)
+end
+
+function skill_init_by_templ(skill, templ)
+    skill.group_kill = templ.group_kill 	--是否群体攻击
+    skill.combo = templ.combo				--是否连击
+	skill.type = templ.type				
+	skill.SkillOnStart = templ.SkillOnStart
+	skill.SkillOnEnd = templ.SkillOnEnd
+	skill.SkillOnHit = templ.SkillOnHit
+	skill.SkillOnSpell = templ.SkillOnSpell
+	skill.SkillOnAfterSpell = templ.SkillOnAfterSpell	
+end
+
+function battle_get_group_kill_targets(battle, skill)
+	return skill.target
+end
+
+function on_skill_start_atk(skill)
+	--[[
+		提前确定单体攻击 还是群体攻击 以及攻击目标
+		默认实现就是单体攻击 
+	]]
+
+	--选定目标
+	skill.atk_targets = {}
+	if skill.group_kill > 0 then
+		skill.atk_targets =  battle_get_group_kill_targets(skill.battle, skill)
+	else
+		skill.atk_targets = { skill.target }
+	end	
+	
+	--单个目标攻击 单个目标伤害计算，连击伤害计算，连击次数计算
+	local perform_cmds = {}
+	for i, target in ipairs(skill.atk_targets) do
+		--[[
+			is combo
+			is dead
+		]]
+		local perform_cmd = {}
+		perform_cmd.type = 'atk'
+		perform_cmd.atk = skill.master
+		perform_cmd.def = skill.target
+		perform_cmd.atk_cnt = 0
+		perform_cmd.atk_perform = {
+			{
+				is_anti_shake = true,
+				is_anti_atk = true,
+				is_suck_blood = true,
+				is_cri_atk = true,
+				def_life_state = 0,
+				is_fly,
+				is_colaps
+			}
+		}
+		table.insert(perform_cmds, perform_cmd)
+		--[[
+			如果target死亡 设置target状态
+			如果targetfly 移除target
+			 
+		]]
+	end
+end
+
+function on_skill_start_spell(skill)
+
+end
+
+function on_skill_start_flee(skill)
+
+end
+
+function on_skill_start_trap(skill)
+
+end
+
+function on_skill_start_def(skill)
+
+end
+
+function on_skill_start(skill)
+	skill.SkillOnStart(skill)
+	if skill.type == 'atk' then
+		on_skill_start_atk(skill)
+	elseif skill.type == 'spell' then
+		on_skill_start_spell(skill)
+	elseif skill.type == 'flee' then
+		on_skill_start_flee(skill)
+	elseif skill.type == 'trap' then
+		on_skill_start_trap(skill)
+	elseif skill.type == 'def' then
+		on_skill_start_def(skill)
+	end
+end
+
 function process_using_skill(battle, master_id, target_id, skill_id)
 	local master = battle:FindActor(master_id)
 	if not master then return end
+	local skill = SkillMT:new()
+	skill.battle = battle
+	skill.tid = skill_id
+	skill.master = master
+
 	local target = battle:FindActor(target_id)
 	if target then
-		master:SetTarget(target)
-	end	
-	master:CastSkill(skill_id)
+		skill.target = target
+	end
+	skill.turn = battle.turn
+
+	skill.templ = skill_table[skill_id]
+	skill_init_by_templ(skill, skill.templ)			
+
+	skill.target_end = false
+	skill.caster_end = false
+	
+	on_skill_start(skill)
+	
 end
 
 function handle_turn_commands(battle)
