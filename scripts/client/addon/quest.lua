4c0ad405611bc7d84dc46706bddabaad7d6770e0
oceancx <oceancx@gmail.com>
2020-02-19 16:51:21 +0800

    help

--- scripts/client/addon/quest.lua
+++ scripts/client/addon/quest.lua
@@ -9,7 +9,11 @@ function ui_show_quest()
     if not ui_is_show_quest then return end
     local player = actor_manager_fetch_local_player()
     if not player then return end
-    imgui.Begin('Quest')
-    imgui.Text('你暂时没有任务！')
+    imgui.Begin('帮助')
+    imgui.Text([[简单说明：
+ALT+W 人物属性编辑器，可以编辑队伍,召唤兽
+ALT+E 染色 换武器 换造型
+ALT+R 战斗的时候会出来，可以控制每个战斗对象的NPC战斗指令
+    ]])
     imgui.End()
 end
