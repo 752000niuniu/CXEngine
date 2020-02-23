18a03d3e6ee17d633a8231e4c8ebddc05dfb9714
oceancx <oceancx@gmail.com>
2020-02-23 21:13:20 +0800

    audio play

--- client/src/audio/audio_manager.cpp
+++ client/src/audio/audio_manager.cpp
@@ -13,45 +13,93 @@
 #define MINIAUDIO_IMPLEMENTATION
 #include <miniaudio.h>
 
+struct AudioFile {
+	CXString path;
+	bool loop;
+	ma_device device;
+	ma_decoder decoder;
+	bool erase;
+	bool start;
+};
+
+std::deque<AudioFile*> g_AudioFiles;
+
 void data_callback(ma_device* pDevice, void* pOutput, const void* pInput, ma_uint32 frameCount)
 {
-	ma_decoder* pDecoder = (ma_decoder*)pDevice->pUserData;
-	if (pDecoder == NULL) {
+	AudioFile* audioFile = (AudioFile*)pDevice->pUserData;
+	if (audioFile == NULL) {
 		return;
 	}
 
-	ma_decoder_read_pcm_frames(pDecoder, pOutput, frameCount);
+	if (!audioFile->erase) {
+		ma_decoder* pDecoder = &audioFile->decoder;
+		ma_uint64 read = ma_decoder_read_pcm_frames(pDecoder, pOutput, frameCount);
+		if (read < frameCount) {
+			if (audioFile->loop) {
+				ma_decoder_seek_to_pcm_frame(pDecoder, 0);
+			}
+			else {
+				pDevice->pUserData = nullptr;
+				audioFile->erase = true;
+			}
+		}
+	}
+
 	(void)pInput;
 }
 
-ma_device device;
-ma_decoder decoder;
-
-
-void audio_manager_stop()
-{
-	ma_device_uninit(&device);
-	ma_decoder_uninit(&decoder);
+void audio_manager_stop(const char* path) {
+	for (auto it = g_AudioFiles.begin(); it != g_AudioFiles.end(); it++) {
+		if ((*it)->path == path) {
+			(*it)->erase = true;
+		}
+	}
 }
 
-int audio_manager_play(const char* path)
+void audio_manager_clear() {
+	for (auto& it : g_AudioFiles) {
+		it->erase = true;
+	}
+}
+
+int audio_manager_play(const char* path, bool loop)
 {
-	audio_manager_stop();
+	if (g_AudioFiles.size() > 0) {
+		for (auto it = g_AudioFiles.begin(); it != g_AudioFiles.end();) {
+			if ((*it)->erase) {
+				ma_device_uninit(&(*it)->device);
+				ma_decoder_uninit(&(*it)->decoder);
+				delete (*it);
+				it = g_AudioFiles.erase(it);
+			}
+			else {
+				it++;
+			}
+		}
+	}
+
+	AudioFile* file = new AudioFile();
+	file->erase = false;
+	file->loop = loop;
+	file->start = false;
+	file->path = path;
+	g_AudioFiles.push_back(file);
+
+	ma_device& device = file->device;
+	ma_decoder& decoder = file->decoder;
 
 	ma_result result;
-	ma_device_config deviceConfig;
-
 	result = ma_decoder_init_file(path, NULL, &decoder);
 	if (result != MA_SUCCESS) {
 		return -2;
 	}
-
+	ma_device_config deviceConfig;
 	deviceConfig = ma_device_config_init(ma_device_type_playback);
 	deviceConfig.playback.format = decoder.outputFormat;
 	deviceConfig.playback.channels = decoder.outputChannels;
 	deviceConfig.sampleRate = decoder.outputSampleRate;
 	deviceConfig.dataCallback = data_callback;
-	deviceConfig.pUserData = &decoder;
+	deviceConfig.pUserData = file;
 
 	if (ma_device_init(NULL, &deviceConfig, &device) != MA_SUCCESS) {
 		printf("Failed to open playback device.\n");
@@ -65,13 +113,22 @@ int audio_manager_play(const char* path)
 		ma_decoder_uninit(&decoder);
 		return -4;
 	}
+
 	return 0;
 };
 
+#define L
+#undef L
+int lua_audio_manager_play(lua_State* L){
+	const char* path = lua_tostring(L, 1);
+	bool loop = lua_toboolean(L, 2);
+	audio_manager_play(path, loop);
+	return 0;
+}
 
-
-void luaopen_audio_manager(lua_State* L_machine)
+void luaopen_audio_manager(lua_State* L)
 {
-	script_system_register_function(L_machine, audio_manager_play);
-	script_system_register_function(L_machine, audio_manager_stop);
+	script_system_register_luac_function_with_name(L,"audio_manager_play", lua_audio_manager_play);
+	script_system_register_function(L, audio_manager_stop);
+	script_system_register_function(L, audio_manager_clear);
 }
