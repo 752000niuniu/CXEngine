5e11970d4e8b216bad42c840bc1c159c8a53694d
oceancx <oceancx@gmail.com>
2020-06-09 14:32:38 +0800

    better

--- cxluax/src/window.cpp
+++ cxluax/src/window.cpp
@@ -14,12 +14,13 @@ static int m_WindowHeight;
 static const float MS_PER_UPDATE = 1000 / 60.f / 1000;
 #define GAME_SCREEN_WIDTH 800
 #define GAME_SCREEN_HEIGHT 600
+static ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
 static void glfw_error_callback(int error, const char* description) {
 	cxlog_err("Error: %d %s\n", error, description);
 }
 
-static GLFWwindow *s_pWindow = nullptr;
+static GLFWwindow* s_pWindow = nullptr;
 bool iw_should_close()
 {
 	return glfwWindowShouldClose(s_pWindow);
@@ -27,6 +28,8 @@ bool iw_should_close()
 
 void iw_begin_render()
 {
+	glfwPollEvents();
+
 	ImGui_ImplOpenGL3_NewFrame();
 	ImGui_ImplGlfw_NewFrame();
 	ImGui::NewFrame();
@@ -45,27 +48,28 @@ void iw_begin_render()
 }
 
 void iw_end_render()
-{ 
+{
 	ImGui::Render();
 	int display_w, display_h;
-	glfwMakeContextCurrent(s_pWindow);
 	glfwGetFramebufferSize(s_pWindow, &display_w, &display_h);
 	glViewport(0, 0, display_w, display_h);
+	glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+	glClear(GL_COLOR_BUFFER_BIT);
 	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-	
+
 	ImGuiIO& io = ImGui::GetIO();
 	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
 	{
+		GLFWwindow* backup_current_context = glfwGetCurrentContext();
 		ImGui::UpdatePlatformWindows();
 		ImGui::RenderPlatformWindowsDefault();
+		glfwMakeContextCurrent(backup_current_context);
 	}
 
-	glfwMakeContextCurrent(s_pWindow);
 	glfwSwapBuffers(s_pWindow);
-	glfwPollEvents();
 }
 
-void iw_init(){
+void iw_init() {
 	if (!glfwInit()) {
 		cxlog_err("glfwInit error!");
 		exit(EXIT_FAILURE);
@@ -88,7 +92,7 @@ void iw_init(){
 	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
 #endif
 	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
-	std::string floatConfig = command_arg_opt_str("window_float","0");
+	std::string floatConfig = command_arg_opt_str("window_float", "0");
 	glfwWindowHint(GLFW_FLOATING, floatConfig == "1");
 
 	s_pWindow = glfwCreateWindow(GAME_SCREEN_WIDTH, GAME_SCREEN_HEIGHT, "CXLUAX", nullptr, nullptr);
@@ -106,6 +110,8 @@ void iw_init(){
 	glfwSetWindowPos(s_pWindow, (monitorW - m_WindowWidth) / 2, (monitorH - m_WindowHeight) / 2);
 
 	glfwMakeContextCurrent(s_pWindow);
+	glfwSwapInterval(1);// Enable vsync
+
 	GLenum err = glewInit();
 	if (GLEW_OK != err) {
 		cxlog_err("glewInit error! %s\n", glewGetErrorString(err));
@@ -114,24 +120,36 @@ void iw_init(){
 	// GLEW generates GL error because it calls glGetString(GL_EXTENSIONS), we'll consume it here.
 	glGetError();
 
-	glfwSwapInterval(1);
 
 	ImGui::CreateContext();
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
-	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+	//io.ConfigViewportsNoAutoMerge = true;
+	//io.ConfigViewportsNoTaskBarIcon = true;
+
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsClassic();
+
+	// When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+	ImGuiStyle& style = ImGui::GetStyle();
+	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	{
+		style.WindowRounding = 0.0f;
+		style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+	}
+
 	ImGui_ImplGlfw_InitForOpenGL(s_pWindow, true);
 	ImGui_ImplOpenGL3_Init(glsl_version);
 
-	ImGui::GetStyle().WindowRounding = 0.0f;
-	ImGui::StyleColorsDark();
-
 	glfwSetInputMode(s_pWindow, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
-	glfwSetWindowOpacity(s_pWindow, 1.0f);
+	//glfwSetWindowOpacity(s_pWindow, 1.0f);
 }
 
-void iw_deinit(){
+void iw_deinit() {
 	ImGui_ImplOpenGL3_Shutdown();
 	ImGui_ImplGlfw_Shutdown();
 	ImGui::DestroyContext();
@@ -141,7 +159,7 @@ void iw_deinit(){
 
 }
 void iw_set_font(const char* path) {
-	ImGuiIO& io = ImGui::GetIO(); 
+	ImGuiIO& io = ImGui::GetIO();
 	io.Fonts->AddFontFromFileTTF(path, 14.0f, NULL, io.Fonts->GetGlyphRangesChineseFull());
 }
 
