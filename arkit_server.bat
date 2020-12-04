365d474d0226128c0429927b1e0aa529588e22f1
top <wangfang@bilibili.com>
2020-12-04 15:43:23 +0800

    arkit_server.bat

--- arkit_server.bat
+++ arkit_server.bat
@@ -1,2 +1,2 @@
 copy /y bin\Debug\cxluax.exe bin\Debug\_cxluax.exe
-start bin\Debug\_cxluax.exe --cwd=%cd%/ --script_path=scripts/launcher/ -Debug
+start bin\Debug\_cxluax.exe --cwd=%cd%/ --script_path=scripts/arkit/ -Debug
